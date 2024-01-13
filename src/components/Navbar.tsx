c00465dc1d9da4476af8e1d1ca3b592978e08c8c
Damian <skrzyniarz.damian@gmail.com>
2024-01-13 12:47:48 +0100

    create Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,32 @@
+function Navbar(){
+    return(
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Grade Book</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                               aria-expanded="false">
+                                Menu
+                            </a>
+                            <ul className="dropdown-menu">
+                                <li><a className="dropdown-item" href="#">Mój Profil</a></li>
+                                <li><a className="dropdown-item" href="#">Przedmioty</a></li>
+                                <li><a className="dropdown-item" href="#">Oceny</a></li>
+
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
